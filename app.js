9f07bb1afe4b47bd3be04f3c3ab4f7b8d03ae23e
dbauszus <dbauszus@geolytix.co.uk>
2016-09-08 18:07:51 +0100

    fixed firefox table-row issue; changed html for ejs templates;

--- app.js
+++ app.js
@@ -21,16 +21,7 @@ app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static(path.join(__dirname + '/public')));
 
-//app.use('/', routes);
-app.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
-});
-app.get('/mobile', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/views/mobile.html'));
-});
-app.get('/map', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/views/map.html'));
-});
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +32,7 @@ app.use(function(req, res, next) {
 
 // development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -51,7 +42,7 @@ if (app.get('env') === 'development') {
 }
 
 // production error handler no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/mobile', function(req, res) {
+  res.render('mobile');
+});
+
+router.get('/map', function(req, res) {
+  res.render('map');
 });
 
 module.exports = router;
