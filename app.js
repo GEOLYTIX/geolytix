8d4928629605b258a36ee691aea988306f4f28ed
dbauszus <dbauszus@geolytix.co.uk>
2016-09-20 11:59:56 +0100

    robots.txt module fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var robots = require('robots.txt')
+var robots = require('robots.txt');
 
 var routes = require('./routes/index');
 
