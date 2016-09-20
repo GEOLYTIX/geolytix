05ced63b93bfb115f51e195a277ba1b03e2d8559
dbauszus <dbauszus@geolytix.co.uk>
2016-09-20 14:51:48 +0100

    update meta tags

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -27,30 +27,30 @@ app.use('/public', express.static(path.join(__dirname + '/public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler no stacktraces leaked to user
-app.use(function(err, req, res) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,39 @@ var router = express.Router();
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport('smtps://geolytix%40gmail.com:123AppleTree@smtp.gmail.com');
 
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', function (req, res) {
+    res.render('index');
 });
 
-router.get('/mobile', function(req, res) {
-  res.render('mobile');
+router.get('/mobile', function (req, res) {
+    res.render('mobile');
 });
 
-router.get('/map', function(req, res) {
-  res.render('map');
+router.get('/map', function (req, res) {
+    res.render('map');
 });
 
-router.get('/robots.txt', function(req, res) {
+router.get('/robots.txt', function (req, res) {
     res.type('text/plain');
     res.send('User-agent: *');
 });
 
 router.get('/contact', function (req, res) {
 
-  var mailOptions = {
-    from: 'geolytix@gmail.com',
-    to: 'info@geolytix.co.uk',
-    subject: 'mail from website',
-    text: req.query.content
-  };
+    var mailOptions = {
+        from: 'geolytix@gmail.com',
+        to: 'info@geolytix.co.uk',
+        subject: 'mail from website',
+        text: req.query.content
+    };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-      return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-    res.json({ok:true});
-  });
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+        res.json({ok: true});
+    });
 
 });
 
