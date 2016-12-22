dcb72f75bb9dc23554e1686bf3d93bdb73cb7f2d
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2016-12-22 17:47:45 +0000

    menu links active;

--- public/js/geolytix.js
+++ public/js/geolytix.js
@@ -1,57 +1,26 @@
-// HEADER
+var header = $('.header__menu'),
+    intro_img = $('#section_intro'),
+    parallax_team_photo = document.getElementById('team_photo');
 
-window.onload = init();
+window.onscroll = function () {
 
-function init() {
-    window.addEventListener('scroll', function(){
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-            //header = document.querySelector('.header__menu');
-            header = $('.header__menu');
-        if (distanceY > 300) {
-            //classie.add(header,"smaller");
-            header.addClass('smaller');
-        } else {
-            // if (classie.has(header, "smaller")) {
-            //     classie.remove(header, "smaller");
-            // }
-            if (header.hasClass('smaller')) {
-                header.removeClass('smaller');
-            }
-        }
-        if (distanceY > $('.img__cover').height()) {
-            //classie.add(header,"smaller");
-            header.addClass('main');
-        } else {
-            // if (classie.has(header, "smaller")) {
-            //     classie.remove(header, "smaller");
-            // }
-            if (header.hasClass('main')) {
-                header.removeClass('main');
-            }
-        }
-    });
-}
+    // TITLE
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    distanceY > 300 ? header.addClass('smaller') : header.removeClass('smaller');
+    distanceY > intro_img.height() - 80 ? header.addClass('main') : header.removeClass('main');
 
-// PARALLAX
-var parallax_team_photo = document.getElementById('team_photo');
-
-(function () {
-    window.onscroll = function () {
-        var shift = 100 - ((parallax_team_photo.offsetTop - window.pageYOffset) / window.innerHeight * 100);
-        if (shift > 0 && shift < 100){
-            var elBackgrounPos = "50% " + shift + "%";
-            parallax_team_photo.style.backgroundPosition = elBackgrounPos;
-        }
-    };
-})();
-
-$(window).resize(function () {
-    if (window.innerWidth <= 799) {
-        window.location = '/mobile';
+    // PARALLAX
+    var shift = 100 - ((parallax_team_photo.offsetTop - window.pageYOffset) / window.innerHeight * 100);
+    if (shift > 0 && shift < 100) {
+        parallax_team_photo.style.backgroundPosition = "50% " + shift + "%";
     }
+};
+
+window.onresize = function () {
+    window.innerWidth <= 799 ? window.location = '/mobile' : null;
     changeExpandedCardsHeight();
     resize_team_photo();
-});
+};
 
 resize_team_photo();
 function resize_team_photo(){
@@ -59,66 +28,48 @@ function resize_team_photo(){
     parallax_team_photo.style.height = width * 0.47 + 'px';
 }
 
+
 // MENU BUTTONS CONTROL
 
-var section_services = $('#section_services'),
+var body = $('body'),
+    section_services = $('#section_services'),
     section_case_studies = $('#section_case_studies'),
     section_geodata = $('#section_geodata'),
     section_team = $('#section_team'),
     section_contact = $('#section_contact'),
     subString = window.location.search.substring(1);
 
-if (subString == 'services') {
-    setTimeout(function () {
-        body__inner.animate({scrollTop: body__inner.scrollTop() + section_services.offset().top - 70});
-    }, 100);
-}
+setTimeout(function () {
+    subString == 'services' ? body.animate({ scrollTop: section_services.offset().top - 100 }) :
+        subString == 'case_studies' ? body.animate({ scrollTop: section_case_studies.offset().top - 50 }) :
+            subString == 'geodata' ? body.animate({ scrollTop: section_geodata.offset().top - 80 }) :
+                subString == 'team' ? body.animate({ scrollTop: section_team.offset().top - 80 }) :
+                    subString == 'contact' ? body.animate({ scrollTop: section_contact.offset().top - 70 }) : null;
+}, 100);
 
-if (subString == 'case_studies') {
-    setTimeout(function () {
-        body__inner.animate({scrollTop: body__inner.scrollTop() + section_case_studies.offset().top - 70});
-    }, 100);
-}
-if (subString == 'geodata') {
-    setTimeout(function () {
-        body__inner.animate({scrollTop: body__inner.scrollTop() + section_geodata.offset().top - 70});
-    }, 100);
-}
-
-if (subString == 'team') {
-    setTimeout(function () {
-        body__inner.animate({scrollTop: body__inner.scrollTop() + section_team.offset().top - 70});
-    }, 100);
-}
-
-if (subString == 'contact') {
-    setTimeout(function () {
-        body__inner.animate({scrollTop: body__inner.scrollTop() + section_contact.offset().top - 70});
-    }, 100);
-}
-
-$('.header__menu > .home').click(function (e) {
+$('#home').click(function(e){
     e.preventDefault();
-    body__inner.animate({ scrollTop: 0 });
+    body.animate({ scrollTop: 0 });
 });
 
-$('.header__menu > .services').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + section_services.offset().top - 70 });
+$('#services').click(function () {
+    body.animate({ scrollTop: section_services.offset().top - 100 });
 });
 
-$('.header__menu > .case_studies').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + section_case_studies.offset().top - 70 });
-});
-$('.header__menu > .geodata').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + section_geodata.offset().top - 70 });
+$('#case_studies').click(function () {
+    body.animate({ scrollTop: section_case_studies.offset().top - 50 });
 });
 
-$('.header__menu > .team').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + section_team.offset().top - 70 });
+$('#geodata').click(function () {
+    body.animate({ scrollTop: section_geodata.offset().top - 80 });
 });
 
-$('.scroll__contact').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + section_contact.offset().top - 70 });
+$('#team').click(function () {
+    body.animate({ scrollTop: section_team.offset().top - 80 });
+});
+
+$('#contact').click(function () {
+    body.animate({ scrollTop: section_contact.offset().top - 70 });
 });
 
 
@@ -153,7 +104,7 @@ section_team__cards.click(expandTeamCard);
 
 section_team.find('.blair').first().trigger('click',[true]);
 
-function expandTeamCard(e, trigger) {
+function expandTeamCard(trigger) {
     if (section_team__expanded_card) {section_team__expanded_card.removeAttr('style')}
     section_team__expanded_card = $(this).parent('li');
     var content = section_team__expanded_card.find('.li_card__content'),
@@ -168,7 +119,7 @@ function expandTeamCard(e, trigger) {
         content.css('opacity', 0).animate({opacity: 1});
         li_expander.css('visibility', 'visible');
         if (trigger != true) {
-            body__inner.animate({ scrollTop: body__inner.scrollTop() + section_team__expanded_card.offset().top - 70 });
+            body.animate({ scrollTop: body.scrollTop() + section_team__expanded_card.offset().top - 70 });
         }
     }
 }
