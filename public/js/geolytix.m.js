1a4569a5b2c669259a6a6a3383b6fcf1ab765ab9
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-01-26 19:43:32 +0000

    mobile view - work in progress;

--- public/js/geolytix.m.js
+++ public/js/geolytix.m.js
@@ -1,12 +1,13 @@
-var intro_text = $('.intro_text');
+var intro__text = $('.intro__text');
+orientation();
 function orientation() {
     if (window.innerHeight < window.innerWidth) {
-        intro_text.html('make better decisions<br>where location matters');
+        intro__text.html('better decisions<br>where location matters');
     } else {
-        intro_text.html('make<br>better<br>decisions<br>where<br>location<br>matters');
+        intro__text.html('better<br>decisions<br>where<br>location<br>matters');
     }
 }
-orientation();
+
 $(window).resize(function () {
     if (window.innerWidth > 799) {
         window.location = "/";
@@ -18,64 +19,18 @@ $(window).resize(function () {
 
 
 
-// CONTACT SCROLL
-var body__inner = $('.inner').first();
-$('.scroll__contact').click(function () {
-    body__inner.animate({ scrollTop: body__inner.scrollTop() + $('#footer__menu').offset().top - 70 });
-});
-
-
-
 // preload images
-var section_intro = $('#section_intro');
-section_intro.find('.img__preload').attr('src', '/public/images/intro_geolytix.jpg').load(function () {
-    $(this).remove();
-    section_intro.css('background-image', 'url(/public/images/intro_geolytix.jpg)');
-    section_intro.find('span').css({
-        'color': '#fff'
-    });
-    setTimeout(function () {
-        section_intro.find('span').css({
-            'text-shadow': '3px 3px 3px #000'
-        });
-    }, 300);
-});
+var img = new Image();
+var section_intro = $('.intro__section');
+img.onload = function(){
+    section_intro.css('background-image', 'url(/public/images/intro_geolytix_m.jpg)');
+    section_intro.find('span').css({'color': '#fff'});
+};
+img.src = '/public/images/intro_geolytix_m.jpg';
 
 
 
-//scroll container
-var scrolly,
-    currentClientCard = 'boots',
-    ClientCard = 'boots',
-    section_clients = $('#section_clients'),
-    section_services = $('#section_services'),
-    scrollContainerClientLogo = section_clients.find('.container').first(),
-    scrollContainerClientInfo = section_clients.find('.container').last(),
-    swapy = window.innerWidth ? window.innerWidth / 2 - 30: $(window).width() / 2 - 30;
-
-scrollContainerClientLogo.on('scrollstop', $.debounce(250, function () {
-    console.log(scrollContainerClientLogo.scrollLeft());
-    scrolly = scrollContainerClientLogo.scrollLeft();
-    scrolly < swapy ? ClientCard = 'johnlewis' :
-        scrolly < swapy * 3 ? ClientCard = 'camelot' :
-            scrolly < swapy * 5 ? ClientCard = 'onthemarket' :
-                scrolly < swapy * 7 ? ClientCard = 'boots' :
-                    scrolly < swapy * 9.5 ? ClientCard = 'totalfitness' :
-                        ClientCard = 'asda';
-    if (currentClientCard != ClientCard) {
-        currentClientCard = ClientCard;
-        section_clients.find('.logo').removeClass('active');
-        scrollContainerClientLogo.find('.' + ClientCard).first().addClass('active');
-        scrollContainerClientInfo.find('.card').hide();
-        scrollContainerClientInfo.find('.' + ClientCard).show();
-        scrollContainerClientInfo.animate({scrollLeft: 0});
-    }
-}));
-
-section_services.find('.scroll__x').first().on('scrollstop', $.debounce(250, function () {
-    console.log(section_services.find('.scroll__x').first().scrollLeft());
-}));
-
+// scroll to substring
 var subString = window.location.search.substring(1);
 if (subString == 'services') {
     setTimeout(function () {
@@ -97,3 +52,38 @@ if (subString == 'team') {
         body__inner.animate({scrollTop: $('#section_team').offset().top});
     }, 100);
 }
+
+
+
+//scroll container
+var scrolly,
+    currentClientCard = 'camelot',
+    ClientCard = 'camelot',
+    section_clients = $('#case_studies__section'),
+    csLogos = $('.case_studies__logos'),
+    csInfo = $('.case_studies__info'),
+    swapy = window.innerWidth ? window.innerWidth / 2 - 30: $(window).width() / 2 - 30;
+
+csInfo.find('.camelot').show();
+
+csLogos.on('scrollstop', $.debounce(250, function () {
+
+    console.log(csLogos.scrollLeft());
+    scrolly = csLogos.scrollLeft();
+
+    scrolly < swapy ? ClientCard = 'camelot' :
+        scrolly < swapy * 3 ? ClientCard = 'johnlewis' :
+            scrolly < swapy * 5 ? ClientCard = 'onthemarket' :
+                scrolly < swapy * 7 ? ClientCard = 'arco' :
+                    scrolly < swapy * 9.5 ? ClientCard = 'totalfitness' :
+                        ClientCard = 'asda';
+
+    if (currentClientCard != ClientCard) {
+        currentClientCard = ClientCard;
+        section_clients.find('.logo').removeClass('active');
+        csLogos.find('.' + ClientCard).first().addClass('active');
+        csLogos.find('.card').hide();
+        csInfo.find('.' + ClientCard).show();
+        csInfo.animate({scrollLeft: 0});
+    }
+}));
