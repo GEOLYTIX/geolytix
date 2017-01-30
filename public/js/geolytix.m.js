0df15a2594468ab42ec82b63cbe0e0fee6055f0a
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-01-30 13:51:55 +0000

    eventlistener;

--- public/js/geolytix.m.js
+++ public/js/geolytix.m.js
@@ -25,11 +25,11 @@ window.addEventListener('resize', function () {
 // preload images
 var img = new Image();
 const section_intro = document.getElementById('intro__section');
-img.onload = function(){
+img.addEventListener('load', function(){
     section_intro.setAttribute('style', 'background-image: url(/public/images/intro_geolytix_m.jpg)');
     intro__logo.setAttribute('style', 'color: #fff');
     intro__text.setAttribute('style', 'color: #fff');
-};
+});
 img.src = '/public/images/intro_geolytix_m.jpg';
 
 
