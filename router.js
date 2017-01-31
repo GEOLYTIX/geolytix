cd3e6d111f55b02ecd615e65c017fdc3de2da116
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-01-31 10:23:22 +0000

    iconst test;

--- router.js
+++ router.js
@@ -13,6 +13,10 @@ router.get('/map', function (req, res) {
     res.render('map');
 });
 
+router.get('/iconst_fail', function (req, res) {
+    res.render('iconst_fail');
+});
+
 router.get('/robots.txt', function (req, res) {
     res.type('text/plain');
     res.send('User-agent: *');
