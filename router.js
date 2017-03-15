fb508b653ac5113d9f60d329618b4f4a65e02e3b
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-03-15 19:19:22 +0000

    mobile map prototype;

--- router.js
+++ router.js
@@ -14,6 +14,10 @@ router.get('/map', function (req, res) {
     res.render('map');
 });
 
+router.get('/map_mobile', function (req, res) {
+    res.render('map_mobile');
+});
+
 router.get('/grid_query', queries.grid_query);
 
 router.get('/iconst_fail', function (req, res) {
