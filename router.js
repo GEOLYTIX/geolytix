0dd7de0af650fcfc013b2b018e7b2b41372986aa
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-04-24 16:49:46 +0100

    mobile detection test;

--- router.js
+++ router.js
@@ -3,9 +3,18 @@ var router = express.Router();
 var queries = require('./queries');
 var jsr = require('jsrender');
 var fs = require('fs');
+var md = require('mobile-detect');
 
 router.get('/', function (req, res) {
-    res.render('index');
+    var _md = new md(req.headers['user-agent']);
+    var _md_mobile = _md.mobile();
+    var _md_phone = _md.phone();
+    var _md_tablet = _md.tablet();
+    if ((_md_mobile === null && _md_phone === null) || _md_tablet !== null) {
+        res.render('index');
+    } else {
+        res.render('mobile');
+    }
 });
 
 router.get('/mobile', function (req, res) {
