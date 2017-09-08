1bd86066e91286f7d0ecab7867927ff5b3dcac5a
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-09-08 19:30:30 +0100

    some jquery bits removed;

--- public/js/geolytix.js
+++ public/js/geolytix.js
@@ -1,5 +1,4 @@
-var body = $('html, body'),
-    header = $('.header'),
+let body = document.querySelector('html, body'),
     header__image = document.getElementById('header__image'),
     parallax_team_photo = document.getElementById('team_photo'),
     section_services = $('.services'),
@@ -11,49 +10,114 @@ var body = $('html, body'),
 parallax_team_photo.style.height = parallax_team_photo.offsetWidth * 0.47 + 'px';
 
 function setHeader() {
-    var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-    distanceY > 300 ? header.addClass('header__smaller') : header.removeClass('header__smaller');
-    distanceY > header__image.clientHeight - 80 ? header.addClass('header__main') : header.removeClass('header__main');
+    let distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    distanceY > 300 ? addClass('.header','header__smaller') : removeClass('.header','header__smaller');
+    distanceY > header__image.clientHeight - 80 ? addClass('.header','header__main') : removeClass('.header','header__main');
 }
 setHeader();
 
 window.onscroll = function () {
     setHeader();
-    var shift = 100 - ((parallax_team_photo.offsetTop - window.pageYOffset) / window.innerHeight * 100);
+    let shift = 100 - ((parallax_team_photo.offsetTop - window.pageYOffset) / window.innerHeight * 100);
     if (shift > 0 && shift < 100) parallax_team_photo.style.backgroundPosition = "50% " + shift + "%";
 };
 
-
 //menu control
-$('#home').click(function (e) {
-    e.preventDefault();
-    body.animate({scrollTop: 0});
-});
+document.getElementById('home').addEventListener('click',
+    function (e) {
+        e.preventDefault();
+        scrollElement(body, 0, 400);
+    }
+);
 
-$('.header__menu > div').click(function () {
-    var loc = $(this).attr('id');
+function scrollElement(element, to, duration) {
+    if (duration <= 0) return;
+    let difference = to - element.scrollTop,
+        perTick = difference / duration * 10;
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) return;
+        scrollElement(element, to, duration - 10);
+    }, 10);
+}
+
+function addClass(elements, myClass) {
+    if (!elements) return;
+
+    // if we have a selector, get the chosen elements
+    if (typeof(elements) === 'string') {
+        elements = document.querySelectorAll(elements);
+    }
+
+    // if we have a single DOM element, make it an array to simplify behavior
+    else if (elements.tagName) { elements=[elements]; }
+
+    // add class to all chosen elements
+    for (let i=0; i<elements.length; i++) {
+        if ((' ' + elements[i].className + ' ').indexOf(' ' + myClass + ' ') < 0) elements[i].className += ' ' + myClass;
+    }
+}
+
+function removeClass(elements, myClass) {
+    if (!elements) return;
+
+    // if we have a selector, get the chosen elements
+    if (typeof(elements) === 'string') {
+        elements = document.querySelectorAll(elements);
+    }
+
+    // if we have a single DOM element, make it an array to simplify behavior
+    else if (elements.tagName) { elements=[elements]; }
+
+    // create pattern to find class name
+    let reg = new RegExp('(^| )'+myClass+'($| )','g');
+
+    // remove class from all chosen elements
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].className = elements[i].className.replace(reg, ' ');
+    }
+}
+
+// $('.header__menu > div').click(function () {
+//     let loc = $(this).attr('id');
+//     history.pushState({so: 'glx'}, loc, '?' + loc);
+//     scrollTo(loc);
+// });
+
+let header__manu_div = document.querySelectorAll('.header__menu > div');
+
+for (let i = 0; i < header__manu_div.length; i++) {
+    header__manu_div[i].addEventListener('click', function () {
+        let loc = this.getAttribute('id');
+        history.pushState({so: 'glx'}, loc, '?' + loc);
+        scrollTo(loc);
+    });
+}
+
+document.querySelector('.header__menu > div').addEventListener('click', function () {
+    let loc = $(this).attr('id');
     history.pushState({so: 'glx'}, loc, '?' + loc);
     scrollTo(loc);
 });
 
 function scrollTo(section) {
-    section === 'services' ? body.animate({scrollTop: section_services.offset().top - 100}) :
-        section === 'case_studies' ? body.animate({scrollTop: section_case_studies.offset().top - 50}) :
-            section === 'geodata' ? body.animate({scrollTop: section_geodata.offset().top - 80}) :
-                section === 'team' ? body.animate({scrollTop: section_team.offset().top - 80}) :
-                    section === 'contact' ? body.animate({scrollTop: section_contact.offset().top - 70}) :
+    section === 'services' ? scrollElement(body, section_services.offset().top - 100, 400) :
+        section === 'case_studies' ? scrollElement(body, section_case_studies.offset().top - 50, 400) :
+            section === 'geodata' ? scrollElement(body, section_geodata.offset().top - 80, 400) :
+                section === 'team' ? scrollElement(body, section_team.offset().top - 80, 400) :
+                    section === 'contact' ? scrollElement(body, section_contact.offset().top - 70, 400) :
                         null;
 }
 
 
 // services cards
-var service_cards = section_services.find('.ul_grid .li_card'),
+let service_cards = section_services.find('.ul_grid .li_card'),
     service_card__expanded;
 
 service_cards.click(function () {
     if (service_card__expanded) service_card__expanded.removeAttr('style');
     service_card__expanded = $(this).parent('li');
-    var content = service_card__expanded.find('.li_card__content'),
+    let content = service_card__expanded.find('.li_card__content'),
         li_expander = $(this).nextAll('.li_expander');
     if (service_card__expanded.hasClass('li_expanded')) {
         service_card__expanded.removeClass('li_expanded').removeAttr('style');
@@ -71,7 +135,7 @@ section_services.find('.network_strategy').first().trigger('click');
 
 
 // case studies
-var case_studies_container = section_case_studies.find('.container').first(),
+let case_studies_container = section_case_studies.find('.container').first(),
     case_studies__strip = section_case_studies.find('.strip').first(),
     case_studies__logos = section_case_studies.find('.logo'),
     case_studies__table = case_studies_container.find('.container_table').first();
@@ -81,18 +145,18 @@ section_case_studies.find('.logo').click(function () {
     $(this).addClass('active');
 
 
-    var index = $(this).index();
+    let index = $(this).index();
     index -= 1;
     case_studies__strip.animate({'marginLeft': '-' + index * 20 + '%'}, 400);
     if (index > 8) {
         index -= 8;
-        var logo = case_studies__logos.get([index+1]);
+        let logo = case_studies__logos.get([index+1]);
         logo.className += " active";
         case_studies__strip.animate({'marginLeft': '-' + index * 20 + '%'}, 0);
     }
     if (index < 0) {
         index += 8;
-        var logo = case_studies__logos.get([index+1]);
+        let logo = case_studies__logos.get([index+1]);
         logo.className += " active";
         case_studies__strip.animate({'marginLeft': '-' + index * 20 + '%'}, 0);
     }
@@ -105,13 +169,13 @@ section_case_studies.find('.logo').click(function () {
 
 
 // team cards
-var team_cards = section_team.find('.ul_grid .li_card'),
+let team_cards = section_team.find('.ul_grid .li_card'),
     team_card__expanded;
 
 team_cards.click(function (e, trigger) {
     if (team_card__expanded) team_card__expanded.removeAttr('style');
     team_card__expanded = $(this).parent('li');
-    var content = team_card__expanded.find('.li_card__content'),
+    let content = team_card__expanded.find('.li_card__content'),
         li_expander = $(this).nextAll('.li_expander');
     if (team_card__expanded.hasClass('li_expanded')) {
         team_card__expanded.removeClass('li_expanded').removeAttr('style');
@@ -132,13 +196,13 @@ section_team.find('.blair').first().trigger('click', [true]);
 
 
 // contact
-var mapZoom_contact = 14,
-map_contact = L.map('map_contact', {
-    scrollWheelZoom: false,
-    zoomControl: false,
-    minZoom: 4,
-    maxZoom: 18
-})
+let mapZoom_contact = 14,
+    map_contact = L.map('map_contact', {
+        scrollWheelZoom: false,
+        zoomControl: false,
+        minZoom: 4,
+        maxZoom: 18
+    })
     .setView([51.52733, -0.11525], mapZoom_contact)
     .addLayer(L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'));
 
@@ -153,7 +217,7 @@ new L.Marker(
     })
     .addTo(map_contact);
 
-var btnZoomIn_contact = document.getElementById('btnZoomIn_contact'),
+let btnZoomIn_contact = document.getElementById('btnZoomIn_contact'),
     btnZoomOut_contact = document.getElementById('btnZoomOut_contact');
 
 function chkZoomBtn_contact(){
