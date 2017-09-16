5357b06e4b0aec3edf14e232b5bbf7233ba68a67
GEOLYTIX <dennis.bauszus@geolytix.co.uk>
2017-09-16 20:04:50 +0100

    black;

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,10 @@ for (let i = 0; i < imgLoadArray.length; i++) {
     let img = new Image();
     img.onload = function(){
         imgLoadArray[i].style['background-image'] = 'url(/images/' + imgLoadArray[i].dataset.src +')';
+        if (imgLoadArray[i].id === 'header__image') {
+            helper.removeClass(document.querySelectorAll('.black'),'black');
+            document.getElementById('intro__text').setAttribute('style', 'color: #fff');
+        }
     };
     img.src = '/images/' + imgLoadArray[i].dataset.src;
 }
@@ -119,40 +123,7 @@ if (document.getElementById('section_case_studies')){
 }
 
 
-
 // geodata
-// let mapZoom = 13;
-// const map = L.map('map_geodata', {
-//     renderer: L.svg(),
-//     scrollWheelZoom: false,
-//     zoomControl: false,
-//     maxBounds: L.latLngBounds(L.latLng(51.35, -0.4), L.latLng(51.65, 0.2)),
-//     minZoom: 12,
-//     maxZoom: 17
-// }).setView([51.50, -0.1], mapZoom);
-//
-// const btnZoomIn = document.getElementById('btnZoomIn');
-// const btnZoomOut = document.getElementById('btnZoomOut');
-//
-// function chkZoomBtn(){
-//     mapZoom < 17 ? btnZoomIn.disabled = false : btnZoomIn.disabled = true;
-//     mapZoom > 12 ? btnZoomOut.disabled = false : btnZoomOut.disabled = true;
-// }
-//
-// btnZoomIn.addEventListener('click', function () {
-//     if (this.disabled) return;
-//     mapZoom++;
-//     map.setZoom(mapZoom);
-//     chkZoomBtn();
-// });
-//
-// btnZoomOut.addEventListener('click', function(){
-//     if (this.disabled) return;
-//     mapZoom--;
-//     map.setZoom(mapZoom);
-//     chkZoomBtn();
-// });
-
 const geodata = require('./geodata')();
 
 geodata.pricing = function() {
