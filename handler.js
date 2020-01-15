1f5499339a509947c4ed58248e69d5af6f2f3c04
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-01-15 13:15:59 +0000

    mapp section for desktop

--- handler.js
+++ handler.js
@@ -19,7 +19,7 @@ module.exports = (req, res) => {
         case_studies: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/case_studies.html`), 'utf8'),
         team: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/team.html`), 'utf8'),
         geodata: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/geodata.html`), 'utf8'),
-        solutions: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/solutions.html`), 'utf8'),
+        mapp: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/mapp.html`), 'utf8'),
         contact: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/contact.html`), 'utf8'),
         awards: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/awards.html`), 'utf8'),
         footer: fs.readFileSync(path.join(process.cwd(), `public/html/${platform}/footer.html`), 'utf8'),
